cb96f17dbdef3c66e099c9032865dfed1d090953
PfLoLexe <volodin3002@gmail.com>
2024-10-27 18:47:48 +0300

    resolve http request and add test request

--- ATLETIUM-T/src/views/ProfilePage.xaml.cs
+++ ATLETIUM-T/src/views/ProfilePage.xaml.cs
@@ -17,13 +17,10 @@ public partial class ProfilePage : ContentPage
     private async void Button_OnClicked(object? sender, EventArgs e)
     {
         HttpClient _client = new HttpClient();
-        Uri uri = new Uri("https://10.0.2.2:8000/hello/Android");
+        Uri uri = new Uri("http://10.0.2.2:8000/hello/Android");
         HttpResponseMessage response = await _client.GetAsync(uri);
-        if (response.IsSuccessStatusCode)
-        {
-            string content = await response.Content.ReadAsStringAsync();
-            new ToastMessage().ShortToast(content);
-        }
-        new ToastMessage().ShortToast("All bad");
+
+        string content = await response.Content.ReadAsStringAsync();
+        new ToastMessage().ShortToast(content);
     }
 }
